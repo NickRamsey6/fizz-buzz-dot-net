b9e70bacdfa3057e034aa1d4cc9e7b1f843b864d
Nick Ramsey <nramseysc@gmail.com>
2024-05-13 09:46:49 -0600

    Add comments

--- Program.cs
+++ Program.cs
@@ -1,13 +1,17 @@
-﻿
+﻿// I removed the System library because looks like later versions of DotNet support top-level functions so I dont need System to write to the console.
 public class Program
 {
     
     public static void Main()
     {
-        Console.WriteLine("Please enter the number you want to count FizzBuzz to:");
+        // With simple console/terminal programs I like to include a little interactivity for the user in the form of console inputs. Adds a little flexibility to the program.
+        Console.WriteLine("Please enter the number you want to count FizzBuzz to (Must be an integer):");
+        // I like the explicit type setting used by C# just like Java. The exception thrown here for non-int values is very simple and intuitive. 
         int countToNum = Convert.ToInt32(Console.ReadLine());
+        // We will start with 1 and count up by 1 up to and including the user's number.
         for (int i = 1; i <= countToNum; i++)
         {
+            // We check for the AND conditional first because otherwise we will never hit it with this setup. If we set divisible by 3 to "Fizz" first then this conditional would be skipped.
             if (i % 3 == 0 && i % 5 == 0)
                 Console.WriteLine("FizzBuzz");
             else if (i % 5 == 0)
